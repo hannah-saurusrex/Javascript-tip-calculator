350511fc848bebb1e153503334debb167723e0e1
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-10-08 16:36:38 -0600

    removed commented out section so that total tip will not be visible on load

--- app/tipcalculator.js
+++ app/tipcalculator.js
@@ -29,8 +29,8 @@ function calculateTip() {
 };
 
 // hide tip amount on load
-// document.getElementById("totalTip").style.display = "none";
-// document.getElementById("each").style.display = "none";
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
 
 // click to call function
 document.getElementById("calculate").onclick = function() {
