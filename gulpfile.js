7c8ccd7a635ddf6e9dc1e73cfc4a50c6c265cc44
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-10-10 14:37:29 -0600

    installed gulp and gulp watch to setup watch tasks

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function() {
+    console.log("hooray, you created a gulp task");
+});
+
+gulp.task('watch', function() {
+    watch('./app/index.html', function() {
+        gulp.series('html');
+    });
+});
