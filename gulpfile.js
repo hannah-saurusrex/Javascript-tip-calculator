dec5326c6826695e6e7a08427efbd29f21fcde77
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-10-11 16:14:24 -0600

    created gulp watch tasks to reload when changes to css, html or javascript files are made

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ function style() {
     // 1. where is my scss file
     return gulp.src('./app/css/main.css')
     // 2. pass that file through the sass compiler
-    .pipe(sass())
+    .pipe(sass().on('error', sass.logError))
     // 3. where do I save the new compiled css file?
     .pipe(gulp.dest('./app/temp/styles'))
     // 4. stream changes to all browsers via browsersync :)
